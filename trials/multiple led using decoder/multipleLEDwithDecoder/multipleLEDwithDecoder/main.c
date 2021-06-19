35f19722c031608a9c0e621de7b395ebeb3d2395
fatimanawmi <fatimanawmi@gmail.com>
2021-06-19 15:05:35 +0600

    Multiple LED with decoder changed

--- trials/multiple led using decoder/multipleLEDwithDecoder/multipleLEDwithDecoder/main.c
+++ trials/multiple led using decoder/multipleLEDwithDecoder/multipleLEDwithDecoder/main.c
@@ -1,7 +1,7 @@
 #define  F_CPU 1000000
 #include <avr/io.h>
 #include <util/delay.h>
-char row[] = {1, 2, 4, 8, 16, 32, 64, 128};
+//char row[] = {1, 2, 4, 8, 16, 32, 64, 128};
 char column1[8][8] = {{0, 0, 1, 1, 1, 0 , 0 , 0},
 					 {0, 0 , 0, 0, 0, 1, 1, 1},
 					 {0, 0, 1, 1, 1, 0 , 0 , 0},
@@ -19,24 +19,38 @@ int main(void)
 	DDRC = 0xFF;
     while (1) 
     {
-		for(int i = 1 ; i <= 8; i++ ){
-			PORTB = i;
+		for(int i = 0 ; i < 8; i++ ){
+			PORTB &= 0b11111000;
+			PORTB |= (i & 0b00000111);
+			
 			for(int j = 0; j < 8; j++){
-				if(column1[i-1][j] == 1){
-					PORTD = ~j;
-					//_delay_ms(10);
+				if(column1[i][j] == 1){
+					PORTD &= 0b11100000;
+					PORTD |= (j & 0b00000111);
+					_delay_us(500);
+				}
+				else{
+					PORTD |= 1 << PD4;
 				}
 			}
 			for(int j = 8; j < 16; j++){
-				if(column1[i-1][j-8] == 0){
-					PORTD = ~j;
-					//_delay_ms(10);
+				if(column1[i][j-8] == 1){
+					PORTD &= 0b11100000;
+					PORTD |= (j & 0b00001111);
+					_delay_us(500);
+				}
+				else{
+					PORTD |= 1 << PD4;
 				}
 			}
 			for(int j = 0; j < 8; j++){
-				if(column1[i-1][j]){
-					PORTC = ~j;
-					//_delay_ms(10);
+				if(column1[i][j] == 1){
+					PORTC &= 0b11110000;
+					PORTC |= (j & 0b00000111);
+					_delay_us(500);
+				}
+				else{
+					PORTC |= 1 << PC3;
 				}
 			}
 			//_delay_ms(10);
