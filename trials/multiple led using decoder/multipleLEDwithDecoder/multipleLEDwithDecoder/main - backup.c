8135d0284b5eb57f4c2c84e196db0b969ea882d3
asifajrof <1705092@ugrad.cse.buet.ac.bd>
2021-06-19 18:35:18 +0600

    delay_try

---
+++ trials/multiple led using decoder/multipleLEDwithDecoder/multipleLEDwithDecoder/main - backup.c
@@ -0,0 +1,65 @@
+#define  F_CPU 1000000
+#include <avr/io.h>
+#include <util/delay.h>
+//char row[] = {1, 2, 4, 8, 16, 32, 64, 128};
+char column1[8][8] = {{0, 0, 1, 1, 1, 0 , 0 , 0},
+					 {0, 0 , 0, 0, 0, 1, 1, 1},
+					 {0, 0, 1, 1, 1, 0 , 0 , 0},
+					 {0, 0 , 0, 0, 0, 1, 1, 1},
+					 {0, 0, 1, 1, 1, 0 , 0 , 0},
+					 {0, 0 , 0, 0, 0, 1, 1, 1},
+					 {0, 0, 1, 1, 1, 0 , 0 , 0},
+					 {0, 0 , 0, 0, 0, 1, 1, 1}} ;	
+						 
+
+int main(void)
+{
+	int delay_D1 = 5000; 
+	int delay_D2 = 1000;
+	int delay_C = 000;
+	int delay__ = 100;
+    DDRB = 0xFF;
+	DDRD = 0xFF;
+	DDRC = 0xFF;
+    while (1) 
+    {
+		for(int i = 0 ; i < 8; i++ ){
+			PORTB &= 0b11111000;
+			PORTB |= (i & 0b00000111);
+			for(int j = 0; j < 8; j++){
+				if(column1[i][j] == 1){
+					PORTD &= 0b11100000;
+					PORTD |= (j & 0b00000111);
+					_delay_us(delay_D1);
+				}
+				else{
+					PORTD |= 1 << PD4;
+				}
+			}
+			
+			for(int j = 8; j < 16; j++){
+				if(column1[i][j-8] == 1){
+					PORTD &= 0b11100000;
+					PORTD |= (j & 0b00001111);
+					_delay_us(delay_D2);
+				}
+				else{
+					PORTD |= 1 << PD4;
+				}
+			}
+			
+			for(int j = 0; j < 8; j++){
+				if(column1[i][j] == 1){
+					PORTC &= 0b11110000;
+					PORTC |= (j & 0b00000111);
+					_delay_us(delay_C);
+				}
+				else{
+					PORTC |= 1 << PC3;
+				}
+			}
+			//_delay_us(delay__);
+		}
+    }
+}
+
--- trials/multiple led using decoder/multipleLEDwithDecoder/multipleLEDwithDecoder/main.c
+++ trials/multiple led using decoder/multipleLEDwithDecoder/multipleLEDwithDecoder/main.c
@@ -11,49 +11,37 @@ char column1[8][8] = {{0, 0, 1, 1, 1, 0 , 0 , 0},
 					 {0, 0, 1, 1, 1, 0 , 0 , 0},
 					 {0, 0 , 0, 0, 0, 1, 1, 1}} ;	
 						 
-	
+
 int main(void)
 {
+	int delay_D1 = 5000; 
+	//int delay_D2 = 1000;
+	//int delay_C = 000;
+	//int delay__ = 100;
     DDRB = 0xFF;
 	DDRD = 0xFF;
 	DDRC = 0xFF;
     while (1) 
     {
-		for(int i = 0 ; i < 8; i++ ){
-			PORTB &= 0b11111000;
-			PORTB |= (i & 0b00000111);
-			
-			for(int j = 0; j < 8; j++){
-				if(column1[i][j] == 1){
-					PORTD &= 0b11100000;
-					PORTD |= (j & 0b00000111);
-					_delay_us(500);
-				}
-				else{
-					PORTD |= 1 << PD4;
-				}
-			}
-			for(int j = 8; j < 16; j++){
-				if(column1[i][j-8] == 1){
-					PORTD &= 0b11100000;
-					PORTD |= (j & 0b00001111);
-					_delay_us(500);
-				}
-				else{
-					PORTD |= 1 << PD4;
+		for(int j = 0 ; j < 16; j++ ){
+			if(j<8){
+				PORTD &= 0b11100000;
+				PORTD |= (j & 0b00000111);
+				for(int i = 0; i < 8; i++){
+					if(column1[i][j] == 1){
+						PORTB &= 0b11111000;
+						PORTB |= (i & 0b00000111);
+						_delay_us(delay_D1);
+					}
+					else{
+						PORTD |= 1 << PD4;
+					}
 				}
 			}
-			for(int j = 0; j < 8; j++){
-				if(column1[i][j] == 1){
-					PORTC &= 0b11110000;
-					PORTC |= (j & 0b00000111);
-					_delay_us(500);
-				}
-				else{
-					PORTC |= 1 << PC3;
-				}
+			else{
+				
 			}
-			//_delay_ms(10);
+			_delay_us(1000);
 		}
     }
 }
